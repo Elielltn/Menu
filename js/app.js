865eb2b2b4e4dddf66c0d618e593d76e1ee63903
Elielltn <liel.lucastj@gmail.com>
2023-09-23 14:57:51 -0300

    manipulando-cardapio-2

--- js/app.js
+++ js/app.js
@@ -15,19 +15,33 @@ cardapio.eventos = {
 cardapio.metodos = {
     
     // obetem a lista de itens do cardápio
-    obterItensCardapio: (categoria = 'burgers') => {
+    obterItensCardapio: (categoria = 'burgers', vermais = false) => {
 
         var filtro = MENU[categoria];
         console.log(filtro);
-
-        $("#itensCardapio").html('')// toda vez que um novo filtro é chamado o filtro anterior é "limpado" e o novo entra no lugar.
+        
+        if (!vermais) {
+            $("#itensCardapio").html('')// toda vez que um novo filtro é chamado o filtro anterior é "limpado" e o novo entra no lugar.
+            $('#btnVerMais').removeClass("hidden")
+        }
+        
+        
         
         $.each(filtro, (i, e) =>{ // Passa em cada um dos itens da Array, pega um template e coloca no item
 
             let temp = cardapio.templates.item.replace(/\${img}/g, e.img)//Utilizando o Regex pra alterar o src padrão do template pelo src de cada item.
             .replace(/\${nome}/g, e.name)
             .replace(/\${preco}/g, e.price.toFixed(2).replace('.', ','));// toFixed() faz com que o float fique com  2 casas decimais e o replace é usado para tirar o ponto e substituir por vírgula.
-            $("#itensCardapio").append(temp)//  Pesquisa pelo ID e Adiciona o template no HTMl
+            
+            // botão ver mais foi clicado (12 itens)
+            if(vermais && i >= 8 && i < 12){
+                $("#itensCardapio").append(temp)//  Pesquisa pelo ID e Adiciona o template no HTMl
+            }
+            
+            // paginação inicial (8 itens)
+            if(!vermais && i < 8){
+                $("#itensCardapio").append(temp)    
+            }
         })
 
         // remove o ativo
@@ -36,6 +50,13 @@ cardapio.metodos = {
         // seta o menu para ativo
         $("#menu-" + categoria).addClass('active')
 
+    },
+
+    verMais: () =>{
+        var ativo = $(".container-menu a.active").attr('id').split('menu-')[1];
+        cardapio.metodos.obterItensCardapio(ativo, true);
+
+        $('#btnVerMais').addClass("hidden")
     },
 };
 
