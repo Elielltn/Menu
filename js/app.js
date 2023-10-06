e2841cc5ba667c4e2450d02ff776524977d31abc
Elielltn <liel.lucastj@gmail.com>
2023-10-06 19:32:32 -0300

    itens-carrinho

--- js/app.js
+++ js/app.js
@@ -135,8 +135,8 @@ cardapio.metodos = {
 
     abrirCarrinho: (abrir) => {
         if (abrir){
-            $("#modalCarrinho").removeClass("hidden")
-            cardapio.metodos.carregarEtapa(1)
+            $("#modalCarrinho").removeClass("hidden");
+            cardapio.metodos.carregarCarrinho();
         } else {
             $("#modalCarrinho").addClass("hidden")
         }
@@ -200,6 +200,61 @@ cardapio.metodos = {
 
     },
 
+    carregarCarrinho: () => {
+        cardapio.metodos.carregarEtapa(1);
+        if(MEU_CARRINHO.length > 0){
+            $("#itensCarrinho").html('');
+            $.each(MEU_CARRINHO, (i, e) => {
+                let temp = cardapio.templates.itemCarrinho.replace(/\${img}/g, e.img)//Utilizando o Regex pra alterar o src padrão do template pelo src de cada item.
+                .replace(/\${nome}/g, e.name)
+                .replace(/\${preco}/g, e.price.toFixed(2).replace('.', ','))// toFixed() faz com que o float fique com  2 casas decimais e o replace é usado para tirar o ponto e substituir por vírgula.
+                .replace(/\${id}/g, e.id)
+                .replace(/\${qntd}/g, e.qntd);
+
+                $("#itensCarrinho").append(temp)
+            })
+        } else {
+            $("#itensCarrinho").html('<p class="carrinho-vazio"><i class="fa fa-shopping-bag"></i>Seu carrinho está vazio</p>')
+        }
+
+    },
+
+    diminuirQuantidadeCarrinho: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+        if(qntdAtual > 1) {
+            $("#qntd-carrinho-" + id).text(qntdAtual - 1);
+            cardapio.metodos.atualizarCarrinho(id, qntdAtual - 1);
+        }
+        else {
+            cardapio.metodos.removerItemCarrinho(id);
+        }
+
+    },
+
+    aumentarQuantidadeCarrinho: (id) => {
+        let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+        $("#qntd-carrinho-" + id).text(qntdAtual + 1);
+        cardapio.metodos.atualizarCarrinho(id, qntdAtual + 1);
+    },
+
+    removerItemCarrinho: (id) => {
+        MEU_CARRINHO = $.grep(MEU_CARRINHO, (e, i) => {return e.id != id})
+        cardapio.metodos.carregarCarrinho();
+
+        cardapio.metodos.atualizarBadgeTotal();
+    },
+
+    atualizarCarrinho: (id, qntd) => {
+
+        let objIndex = MEU_CARRINHO.findIndex((obj => obj.id == id));
+        MEU_CARRINHO[objIndex].qntd = qntd;
+
+        cardapio.metodos.atualizarBadgeTotal();
+
+    },
+
+
     mensagem: (texto, cor = 'red', tempo = 3500) =>{
         let id = Math.floor(Date.now() * Math.random()).toString();
         let msg = `<div id="msg-${id}" class="animated fadeInDown toast ${cor}">${texto}</div>`;
@@ -238,6 +293,26 @@ cardapio.templates = {
                 </div>
             </div>
         </div>
+    `,
+
+    itemCarrinho: `
+        <div class="col-12 item-carrinho">
+            <div class="img-produto">
+                <img src="\${img}">
+            </div>
+            <div class="dados-produto">
+                <p class="title-produto"><b>\${nome}</b></p>
+                <p class="price-produto"><b>R$ \${preco}</b></p>
+            </div>
+            <div class="add-carrinho">
+                <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidadeCarrinho('\${id}')"><i class="fas fa-minus"></i></span>
+                <span class="add-numero-itens" id="qntd-carrinho-\${id}">\${qntd}</span>
+                <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')"><i class="fas fa-plus"></i></span>
+                <span class="btn btn-remove" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="fa fa-times "></i></span>
+            </div>
+        </div>  
     `
+
+
 
 };
